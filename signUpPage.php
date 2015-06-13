78dd6a9975c65bda755be29ff632124e397d2e10
Sky3801 <sky930227@gmail.com>
2015-06-13 16:11:34 -0700

    Added php files.

---
+++ signUpPage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans" />
+		<link rel="stylesheet" type="text/css" href="loginStyle.css" />
+		<title> Sign up account -- Data Bank </title>
+	</head>
+	<body>
+		<h1 class="specialHeader"> Sign up </h1>
+		<div>
+			<form action="#" class="signUpForm">
+				<p> Username </p>
+				<input type="text" placeholder="John" class="loginInput" />
+				<p> Email </p>
+				<input type="text" placeholder="John@example.com" class="loginInput" />
+				<p> Password </p>
+				<input type="password" class="loginInput" />
+				<p> Confirm Password </p>
+				<input type="password" class="loginInput" />
+				<input type="submit" value="Sign up" />
+			</form>
+		</div>
+		<p> By singning up, you agree to Data Sharer's <a href="#" class="terms"> Terms of Service </a> </p>
+	</body>
+</html>
