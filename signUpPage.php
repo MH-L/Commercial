06c55f12ff9346125f0f2d319909e0390449bcf4
MH-L <922lmh922@gmail.com>
2015-06-15 15:01:13 +0800

    Delete ini files and added fields to account info.

--- signUpPage.php
+++ signUpPage.php
@@ -9,17 +9,25 @@
 		<h1 class="specialHeader"> Sign up </h1>
 		<div>
 			<form action="#" class="signUpForm">
+				<p> Last Name </p>
+				<input type="text" placeholder="Smith" class="loginInput" name="lName" />
+				<p> First Name </p>
+				<input type="text" placeholder="John" class="loginInput" name="fName" />
 				<p> Username </p>
-				<input type="text" placeholder="John" class="loginInput" />
+				<input type="text" placeholder="John000" class="loginInput" name="uName" />
 				<p> Email </p>
-				<input type="text" placeholder="John@example.com" class="loginInput" />
+				<input type="text" placeholder="John@example.com" class="loginInput" name="uEmail" />
 				<p> Password </p>
-				<input type="password" class="loginInput" />
+				<input type="password" class="loginInput" name="uPassword" />
 				<p> Confirm Password </p>
-				<input type="password" class="loginInput" />
+				<input type="password" class="loginInput" name="confirmPassword" />
 				<input type="submit" value="Sign up" />
 			</form>
 		</div>
-		<p> By singning up, you agree to Data Sharer's <a href="#" class="terms"> Terms of Service </a> </p>
+		<p> By signing up, you agree to Data Sharer's <a href="#" class="terms"> Terms of Service </a> </p>
 	</body>
 </html>
+
+<?php 
+
+?>
