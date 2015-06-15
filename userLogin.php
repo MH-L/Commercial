2744d2a2f4d5256190a447cfe04c544315e97c5f
MH-L <922lmh922@gmail.com>
2015-06-15 14:37:52 +0800

    Finished the login page construction.

--- userLogin.php
+++ userLogin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML>
 <html>
+
 	<head>
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans" />
 		<link rel="stylesheet" type="text/css" href="loginStyle.css" />
@@ -7,10 +8,10 @@
 	</head>
 	<body>
 		<h1> Sign in </h1>
-		<form method="post" action="userLogin.php">
+		<form method="POST" action="#">
 			<input type="text" placeholder="User name or email:" class="loginInput" name="uname" />
 			<input type="password" placeholder="Password" class="loginInput" name="upassword" />
-			<input type="submit" value="Login" name="submitBtn" />
+			<input type="submit" value="Login" name="submitBtn" href="#"/>
 		</form>
 		<a href="passwordReset.html"> Forget password? </a>
 	</body>
@@ -20,14 +21,12 @@
 <?php
 
 $mysql_hostname = "107.170.192.241";
-$mysql_username = "root";
-$mysql_password = "51102135";
-$mysql_database = "account";
-$db = mysql_connect ($mysql_hostname,$mysql_username,$mysql_password);
+$mysql_username = "lmh";
+$mysql_password = "88781770";
+$mysql_database = "login";
+$db = mysql_connect($mysql_hostname,$mysql_username,$mysql_password) or die (mysql_error());
 
-or die ("Something is wrong");
-
-mysql_select_db($mysql_database, $db) or die ("Couldn't find database");
+mysql_select_db($mysql_database, $db) or die (mysql_error());
 
 $username_entered = $_POST["uname"];
 $password_entered = $_POST["upassword"];
@@ -38,6 +37,16 @@ if ($submit_btn) {
 		echo "Please input a username.";
 	} else if ($password_entered == "") {
 		echo "Please enter your password.";
+	} else {
+		$query_str = "select password from account where name = \"" . $username_entered . "\";";
+		$result = mysql_query($query_str) or die (mysql_error());
+		$row = mysql_fetch_array($result, MYSQL_NUM);
+		if ($row[0] == $password_entered) {
+			echo "<script type=\"text/javascript\"> window.location.replace(\"http://www.datasharer.com/accountPage.html\"); </script>";
+		} else {
+			echo "<script type=\"text/javascript\"> alert(\"Incorrect password!\"); </script>";
+			// echo "<script type=\'text/javascript\'> alert(\'Wrong username/password combination!\'); </script>";
+		}
 	}
 }
 
